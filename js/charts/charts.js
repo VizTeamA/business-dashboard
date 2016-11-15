0974da1bb47a1b8d89b1a5d1bc70294225237814
Tien Duong <tienduong.nk@gmail.com>
2016-11-15 17:39:24 +0800

    Improve interaction
    Imprv Interaction
    Add some indicators

--- js/charts/charts.js
+++ js/charts/charts.js
@@ -6,6 +6,7 @@ var inputFile2 = 'data/hotelsg-sales.csv';
 var inputFileDemo = "demo1.tsv";
 var inputSaleTrans = 'data/tables/SALES_TRANS.csv';
 var productChart = dc.rowChart("#chart-top .product", groupname);
+var saleBulletChart = d3.bullet();
 
 <!-- Data input -->
 var targetDataJson = [
@@ -20,11 +21,9 @@ d3.csv(inputSaleTrans, function(data) {
   xfProductSaleData = crossfilter(data);
   drawProductBarChart(xfProductSaleData);
   drawSaleBulletChart(xfProductSaleData);
-
   d3.csv(inputFile2, function(data) {
     drawMapChart(data);
   });
-
 });
 
 
@@ -51,18 +50,21 @@ function drawProductBarChart(xfProductSaleData) {
 	var saleCol = 'Sales';
 	var yearCol = 'Year';
 
-	 products = xfProductSaleData.dimension(function(d) {return d[productCol]});
-	 productSales = products.group().reduceSum( function(d) {return d[saleCol]});
-   yearDim = xfProductSaleData.dimension(function(d) {return d["Year"]});
+	products = xfProductSaleData.dimension(function(d) {return d[productCol]});
+	productSales = products.group().reduceSum( function(d) {return d[saleCol]});
+	yearDim = xfProductSaleData.dimension(function(d) {return d["Year"]});
 
 	productChart
 		.dimension(products)
 		.group(productSales)
+		.on("click",function(d) {console.log("Pressed");  })
 		.width(500)
-    .elasticX(true)
+		.elasticX(true)
 		.xAxis().ticks(5);
 	dc.renderAll(groupname);
 
+	
+	
 	function AddXAxis(chartToUpdate, displayText, offsetY) {
 	chartToUpdate.svg()
 				.append("text")
@@ -83,57 +85,77 @@ function updateProductChart(year) {
 }
 
 function drawSaleBulletChart (datacf) {
-  var margin = {top: 5, right: 5, bottom: 50, left: 30},
-      width = 80 - margin.left - margin.right,
-      height = 250 - margin.top - margin.bottom;
+	var margin = {top: 5, right: 5, bottom: 50, left: 30},
+	  width = 80 - margin.left - margin.right,
+	  height = 250 - margin.top - margin.bottom;
 
-  var saleBulletChart = d3.bullet()
-      .orient("bottom")
-      .width(width)
-      .height(height);
+	//var saleBulletChart = d3.bullet()
+	saleBulletChart	
+	  .orient("bottom")
+	  .width(width)
+	  .height(height);
 
 
-d3.csv("./data/tables/TARGET.csv", function(data) {
-  var targetDataJson = [
-    {"Year":"2012","ranges":[150,225,300],"measures":[220,270],"markers":[250]},
-    {"Year":"2013","ranges":[150,225,300],"measures":[220,230],"markers":[240]},
-    {"Year":"2014","ranges":[150,225,300],"measures":[210,230],"markers":[260]},
-    {"Year":"2015","ranges":[150,225,300],"measures":[200,210],"markers":[230]},
-    {"Year":"2016","ranges":[150,225,300],"measures":[220,260],"markers":[290]},
-  ];
+	d3.csv("./data/tables/TARGET.csv", function(data) {
+	  var targetDataJson = [
+		{"Year":"2012","ranges":[150,225,300],"measures":[220,270],"markers":[250]},
+		{"Year":"2013","ranges":[150,225,300],"measures":[220,230],"markers":[240]},
+		{"Year":"2014","ranges":[150,225,300],"measures":[210,230],"markers":[260]},
+		{"Year":"2015","ranges":[150,225,300],"measures":[200,210],"markers":[230]},
+		{"Year":"2016","ranges":[150,225,300],"measures":[220,260],"markers":[290]},
+	  ];
 
-  var datacf       	= crossfilter(targetDataJson),
-      titleDimension 	= datacf.dimension(function(d) {return d.Year;}),
-      statusGroup    	= {
-        all: function(){
-          return dataExample;
-      }};
+	  var datacf       	= crossfilter(targetDataJson),
+		  titleDimension 	= datacf.dimension(function(d) {return d.Year;}),
+		  statusGroup    	= {
+			all: function(){
+			  return dataExample;
+		  }};
 
-  data = titleDimension.top(Infinity);
+	  data = titleDimension.top(Infinity);
 
-  var salesYearBulletChart = d3.select("#chart-top .sales-year-chart").selectAll("svg")
-    .data(data)
-  .enter().append("svg")
-    .attr("class", "bullet")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .on("click", function(d) {
-      updateProductChart(d.Year);
-    })
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    .call(saleBulletChart);
+	  bulletChartSvg = d3.select("#chart-top .sales-year-chart").selectAll("svg")
+		.data(data)
+	  .enter().append("svg")
+		.attr("class", "bullet")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.on("click", function(d) {
+		  updateProductChart(d.Year);
+		})
+	  .append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+		.call(saleBulletChart);
 
-  var title = salesYearBulletChart.append("g")
-    .style("text-anchor", "end")
-    .attr("transform", "translate(" + width + "," + (height + 20) + ")");
+	  var title = bulletChartSvg.append("g")
+		.style("text-anchor", "end")
+		.attr("transform", "translate(" + width + "," + (height + 20) + ")");
 
-  title.append("text")
-    .attr("class", "title")
-    .text(function(d) { return d.Year; })
-    .on("click", function(d) {
-  	alert(d.title);
-    });
-});
+	  title.append("text")
+		.attr("class", "title")
+		.text(function(d) { return d.Year; })
+		.on("click", function(d) {
+		alert(d.title);
+		});
 
+	  d3.selectAll("button").on("click", function() {
+		bulletChartSvg.datum(randomize).transition().duration(1000).call(saleBulletChart);
+	  });		
+
+	});
+}
+
+function randomize(d) {
+  if (!d.randomizer) d.randomizer = randomizer(d);
+  d.ranges = d.ranges.map(d.randomizer);
+  d.markers = d.markers.map(d.randomizer);
+  d.measures = d.measures.map(d.randomizer);
+  return d;
+}
+
+function randomizer(d) {
+  var k = d3.max(d.ranges) * .2;
+  return function(d) {
+    return Math.max(0, d + k * (Math.random() - .5));
+  };
 }
